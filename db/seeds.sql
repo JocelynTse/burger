ca26e07d78cb994229a7b635c5dfb7c2b0fc9ef3
JocelynTse <jocelyn.s.tse@gmail.com>
2018-08-04 00:41:59 -0500

    created files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('hamburger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('veggie burger', false);
